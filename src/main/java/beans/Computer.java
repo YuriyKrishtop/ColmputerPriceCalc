64a16ce7a74d34423ed48d1f243f2f111f3a6d07
Iurii Kryshtop <iurii_kryshtop@epam.com>
2016-08-30 15:37:42 +0300

    fixed getters and setters

--- src/main/java/beans/Computer.java
+++ src/main/java/beans/Computer.java
@@ -13,59 +13,59 @@ public class Computer {
     private double price;
 
 
-//    public int getCpu() {
-//        return cpu;
-//    }
-//
-//    public void setCpu(int cpu) {
-//        this.cpu = cpu;
-//    }
-//
-//    public int getRam() {
-//        return ram;
-//    }
-//
-//    public void setRam(int ram) {
-//        this.ram = ram;
-//    }
-//
-//    public int getHdd() {
-//        return hdd;
-//    }
-//
-//    public void setHdd(int hdd) {
-//        this.hdd = hdd;
-//    }
-//
-//    public int getSsd() {
-//        return ssd;
-//    }
-//
-//    public void setSsd(int ssd) {
-//        this.ssd = ssd;
-//    }
-//
-//    public int getVideo() {
-//        return video;
-//    }
-//
-//    public void setVideo(int video) {
-//        this.video = video;
-//    }
-//
-//    public int getPower() {
-//        return power;
-//    }
-//
-//    public void setPower(int power) {
-//        this.power = power;
-//    }
-//
-//    public double getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(double price) {
-//        this.price = price;
-//    }
+    public int getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(int cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(int ssd) {
+        this.ssd = ssd;
+    }
+
+    public int getVideo() {
+        return video;
+    }
+
+    public void setVideo(int video) {
+        this.video = video;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
